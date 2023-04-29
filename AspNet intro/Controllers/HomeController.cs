dadddc8b925a79b0cd4269b60f99682904b70b6a
anarmh <anarmh@code.edu.az>
2023-04-29 22:30:10 +0400

    add practice

---
+++ AspNet intro/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNet_intro.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            int[] nums = {1,2,3,4,5,6};
+            return View(nums);
+            //return Json("Salamlar");
+            //return Json(new {name="Anar",surname="Huseynov"});
+            //return Ok("success");
+            //return NotFound();
+            //return BadRequest();
+        }
+    }
+}
